ad7c9bdbc107e890cfda3c2e5339c7b498d14f8f
Utku <utkutrn@hotmail.com>
2023-10-25 23:05:37 +0300

    first commit

---
+++ üçsayı.py
@@ -0,0 +1,8 @@
+sayi1 = float(input("Birinci sayıyı girin :"))
+sayi2 =float(input("İkinci sayıyı girin :"))
+sayi3 =float(input("Üçüncü saıyı girin :" ))
+
+
+en_buyuk_sayi = max(sayi1 , sayi2 , sayi3)
+
+print("En büyük sayı:", en_buyuk_sayi)
