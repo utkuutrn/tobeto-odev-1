66c3a6f27e884db886b9300bed0020015d9274e6
Utku <utkutrn@hotmail.com>
2023-10-25 23:09:59 +0300

    rename uc sayi

--- üçsayı.py
+++
@@ -1,8 +0,0 @@
-sayi1 = float(input("Birinci sayıyı girin :"))
-sayi2 =float(input("İkinci sayıyı girin :"))
-sayi3 =float(input("Üçüncü saıyı girin :" ))
-
-
-en_buyuk_sayi = max(sayi1 , sayi2 , sayi3)
-
-print("En büyük sayı:", en_buyuk_sayi)
